7ba788ec55a1f67be148e0605ff10cebd404fdeb
wlsf82 <wlsf82@gmail.com>
2015-06-09 01:57:50 -0300

    Added experiment test into protractor configuration file.

--- conf.js
+++ conf.js
@@ -1,5 +1,16 @@
 // conf.js
 exports.config = {
+
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js']
+
+  specs: [
+            'spec.js',
+            'spec.experiment.js'
+         ],
+
+  jasmineNodeOpts: {
+
+    defaultTimeoutInterval: 999999
+
+  }
 }
